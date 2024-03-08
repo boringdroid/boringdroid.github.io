1c22202d8532c19db46129a47e3824dff72b9b3b
utzcoz <utzcoz@outlook.com>
2024-03-08 23:32:03 +0800

    Apply format
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- js/navbar-ontop.js
+++ js/navbar-ontop.js
@@ -1,17 +1,25 @@
 /**
  * navbar-ontop.js 1.0.0
- * Add .navbar-ontop class to navbar when the page is scrolled to top
- * Make sure to add this script to the <head> of page to avoid flickering on load
+ * Add .navbar-ontop class to navbar when the page is
+ * scrolled to top.
+ * Make sure to add this script to the <head> of page
+ * to avoid flickering on load.
  */
 
 (function() {
   const className = 'navbar-ontop';
 
   // we start hidden, to avoid flickering
-  document.write('<style id=\'temp-navbar-ontop\'>.navbar {opacity:0; transition: none !important}</style>');
+  document.write(
+      '<style id=\'temp-navbar-ontop\'>' +
+      '.navbar {opacity:0; transition: none !important}' +
+      '</style>',
+  );
 
+  /**
+   * Toggle className based on the scrollTop property of document.
+   */
   function update() {
-    // toggle className based on the scrollTop property of document
     const nav = document.querySelector('.navbar');
 
     if (window.scrollY > 30) {
@@ -23,7 +31,9 @@
 
   document.addEventListener('DOMContentLoaded', function(event) {
     $(window).on('show.bs.collapse', function(e) {
-      $(e.target).closest('.' + className).removeClass(className);
+      $(e.target)
+          .closest('.' + className)
+          .removeClass(className);
     });
 
     $(window).on('hidden.bs.collapse', function(e) {
@@ -34,7 +44,7 @@
     setTimeout(function() {
       document.querySelector('#temp-navbar-ontop').remove();
     });
-  	});
+  });
 
   window.addEventListener('scroll', function() {
     update();
--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -14,32 +14,40 @@
       .click(function(event) {
       // On-page links
         if (
-          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-        location.hostname == this.hostname
+          location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
+        location.hostname == hostname
         ) {
         // Figure out element to scroll to
           let target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          target = target.length ?
+          target :
+          $('[name=' + this.hash.slice(1) + ']');
           // Does a scroll target exist?
           if (target.length) {
           // Only prevent default if animation is actually gonna happen
             event.preventDefault();
 
-            $('html, body').animate({
-              scrollTop: target.offset().top,
-            },
-            duration, function() {
-            // Callback after animation
-            // Must change focus!
-              const $target = $(target);
-              $target.focus();
-              if ($target.is(':focus')) { // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              };
-            });
+            $('html, body').animate(
+                {
+                  scrollTop: target.offset().top,
+                },
+                duration,
+                function() {
+                  // Callback after animation
+                  // Must change focus!
+                  const $target = $(target);
+                  $target.focus();
+                  if ($target.is(':focus')) {
+                    // Checking if the target was focused
+                    return false;
+                  } else {
+                    // Adding tabindex for elements not focusable
+                    $target.attr('tabindex', '-1');
+                    $target.focus(); // Set focus again
+                  }
+                },
+            );
           }
         }
       });
